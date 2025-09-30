ce8b3da8d4297403fad55ce9f3cfc632a14f8abc
Prince Verma <tovermaprince@gmail.com>
2025-09-30 16:53:43 +0530

    Create data_preprocessing.py

---
+++ src/data_preprocessing.py
@@ -0,0 +1,15 @@
+"""
+Data preprocessing utilities for road sign detection
+"""
+
+def parse_annotations(xml_file):
+    """Parse XML annotations in PASCAL VOC format"""
+    pass
+
+def load_images(image_paths):
+    """Load and preprocess images"""
+    pass
+
+def create_dataframe(annotations, images):
+    """Create training dataframe from annotations and images"""
+    pass
