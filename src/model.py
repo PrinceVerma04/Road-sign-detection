e87621a503fe95c43628994f1d0be611e2661420
Prince Verma <tovermaprince@gmail.com>
2025-09-30 16:54:13 +0530

    Create model.py

---
+++ src/model.py
@@ -0,0 +1,11 @@
+"""
+Model architecture for road sign detection
+"""
+
+def create_model():
+    """Create and return the road sign detection model"""
+    pass
+
+def train_model(model, train_data, val_data):
+    """Train the road sign detection model"""
+    pass
