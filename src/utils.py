01580d4e0f531055e369bb5cbd87d14a29f60b31
Prince Verma <tovermaprince@gmail.com>
2025-09-30 16:54:31 +0530

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+"""
+Utility functions for road sign detection
+"""
+
+def display_sample_images(image_paths, num_samples=3):
+    """Display sample images from the dataset"""
+    pass
+
+def calculate_metrics(predictions, ground_truth):
+    """Calculate evaluation metrics"""
+    pass
